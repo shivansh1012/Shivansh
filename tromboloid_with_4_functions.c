03fc3bf9faff29884f6d9ee5c0439a1dc63d1063
Shivansh Pandey <53964760+shivansh1012@users.noreply.github.com>
2021-02-05 00:21:21 +0530

    Activity 5

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,24 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include <stdio.h>
+double h,b,d;
+void input() {
+    printf("Enter value of h:");
+    scanf("%lf",&h);
+    printf("Enter value of d:");
+    scanf("%lf",&d);
+    printf("Enter value of b:");
+    scanf("%lf",&b);
+}
+
+double VolumeOfTromboloid(double h, double b, double d) { 
+    if(b==0) return 0;
+    else return ((1/(double)3)*((h*d*b)+(d/b))); 
+    }
+
+void output(double Volume) { printf("Volume of Tromboloid %lf\n",Volume); }
+
+int main() {
+    input();
+    output(VolumeOfTromboloid(h,b,d));
+    return 0;
+}
