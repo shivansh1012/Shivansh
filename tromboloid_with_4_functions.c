d6e44bc5642ede6a8542513602d55d997325488f
Shivansh Pandey <53964760+shivansh1012@users.noreply.github.com>
2021-02-05 12:01:00 +0530

    Activity 5

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 double h,b,d;
 void input() {
-    printf("Enter value of h:");
+    printf("Enter value of h of Tromboloid:");
     scanf("%lf",&h);
-    printf("Enter value of d:");
+    printf("Enter value of d of Tromboloid:");
     scanf("%lf",&d);
-    printf("Enter value of b:");
+    printf("Enter value of b of Tromboloid:");
     scanf("%lf",&b);
 }
 
-double VolumeOfTromboloid(double h, double b, double d) { 
+double VolumeOfTromboloid() { 
     if(b==0) return 0;
     else return ((1/(double)3)*((h*d*b)+(d/b))); 
     }
@@ -19,6 +19,6 @@ void output(double Volume) { printf("Volume of Tromboloid %lf\n",Volume); }
 
 int main() {
     input();
-    output(VolumeOfTromboloid(h,b,d));
+    output(VolumeOfTromboloid());
     return 0;
 }
