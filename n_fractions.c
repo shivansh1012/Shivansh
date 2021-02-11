2a27a0e0539fdd1fa3ebe4047a0c4bcf75b2e7b7
Shivansh Pandey <53964760+shivansh1012@users.noreply.github.com>
2021-02-11 11:58:17 +0530

    Acttivity 10

--- n_fractions.c
+++ n_fractions.c
@@ -1,42 +1,76 @@
-//WAP to find the sum of n fractions.
 #include <stdio.h>
-int len;
 struct fraction
 {
-    float num,den;
+    int num, den;
 };
 typedef struct fraction Fraction;
 
-Fraction input() {
-    Fraction p;
-    printf("Enter the numerator ");
-    scanf("%f",&p.num);
-    printf("Enter the denominator: ");
-    scanf("%f",&p.den);
-    return p;
+Fraction addition(Fraction f1, Fraction f2)
+{
+	Fraction res;
+	res.den = f1.den * f2.den;
+	res.num = (f1.num * f2.den) + (f2.num * f1.den);
+
+	return res;
 }
 
-void input_arr(Fraction a[len]) {
-    for(int i=0;i<len;i++){
-        a[i]=input();
-    }
+Fraction sum(Fraction a[], int size)
+{
+    Fraction total;
+	total.num = 0;
+	total.den = 1;
+
+	for (int i = 0; i < size; i++)
+		total = addition(total, a[i]);
+
+	return total;
 }
 
-float sum(Fraction a) { 
-    float d;
-    d=a.num/a.den;
-    return d;
+int gcd(int num, int den)
+{
+	int div;
+	for (int i = 1; i <= num && i <= den; i++)
+	{
+		if (num % i == 0 && den % i == 0)
+			div = i;
+	}
+	return div;
 }
 
-void output(float d) { printf("The sum is %f\n",d); }
+Fraction reduce_lowest(Fraction f, int divisor)
+{
+    f.num /= divisor;
+	f.den /= divisor;
 
-int main() {
+    return f;
+}
+
+void output(Fraction f, int divisor)
+{
+	printf("The answer is: %d/%d", f.num, f.den);
+}
+
+
+int main()
+{
+    int len, divisor;
+	Fraction result;
+
     printf("Enter Total numbers\n");
-    scanf("%d",&len);
+    scanf("%d", &len);
     Fraction a[len];
-    input_arr(a);
-    float total=0;
-    for (int i=0;i<len;i++) total+=  sum(a[i]);
-    output(total);
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("Enter the numerator ");
+        scanf("%d", &a[i].num);
+        printf("Enter the denominator: ");
+        scanf("%d", &a[i].den);
+    }
+    result = sum(a, len);
+    divisor = gcd(result.num, result.den);
+	result = reduce_lowest(result, divisor);
+	output(result, divisor);
     return 0;
+
 }
