c35a22e50b82092deaa5eb89cfe1108ebc3d25b3
Shivansh Pandey <53964760+shivansh1012@users.noreply.github.com>
2021-02-12 11:41:06 +0530

    Activity 9

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-void input(int len, int a[len]) {
+int getLength() {
+	int len;
+	printf("Enter Total numbers\n");
+    scanf("%d", &len);
+	return len;
+}
+
+void getInput(int len, int a[len]) {
     for (int i = 0; i < len; i++) {
         printf("Enter the element no %d of the array\n", i + 1);
         scanf("%d", &a[i]);
     }
 }
 
-int sum(int len, int a[len]) {
+int computeSum(int len, int a[len]) {
     int total = 0;
     for (int i = 0; i < len; i++) {
         total += a[i];
@@ -15,15 +22,19 @@ int sum(int len, int a[len]) {
     return total;
 }
 
-int output(int total) {
-    printf("The Sum of all the numbers is %d\n", total);
+int output(int len, int a[len], int total) {
+    int i;
+    printf("The Sum of");
+    for(i=0;i<len-1;i++) {
+        printf(" %d +",a[i]);
+    }
+    printf("%d is %d\n", a[i], total);
 }
 
 int main() {
-    int len;
-    printf("Enter Total numbers\n");
-    scanf("%d", &len);
+    int len=getLength(), total;
     int a[len];
-    input(len, a);
-    output(sum(len, a));
+    getInput(len, a);
+    total=computeSum(len, a);
+    output(len, a, total);
 }
